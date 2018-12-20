1c56a2d4f1d1bf82258b316653f4daaf8c102756
G4-WebCETAV <grupo4pic@gmail.com>
2018-12-19 22:10:20 -0600

    fixing google chrome speak deprecation

--- js/script.js
+++ js/script.js
@@ -4,7 +4,14 @@ let alfredListening = new p5.SpeechRec('en-GB', listenTheCreator);
 alfredListening.continous = true;
 alfredListening.interimResults = true;
 
-alfredListening.start(); 
-wi(()=>{
-   alfredListening.start(); 
-}, 4000);
+let seeIfClick = false;
+
+document.addEventListener('click', ()=> {
+    if(!seeIfClick){
+        alfredListening.start(); 
+        wi(()=>{
+        alfredListening.start(); 
+        }, 4000);
+        seeIfClick = true;
+    }
+});
